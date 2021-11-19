4c610000236814c277f8110f66b0297aa7087d5f
HJ Suh <hj.suh@databricks.com>
2021-11-19 14:42:02 -0800

    folders

---
+++ folder3/test_db_notebook2.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+1 + 1
+
+# COMMAND ----------
+
+2 + 2
+
+# COMMAND ----------
+
+print("Hello World!")
+
+# COMMAND ----------
+
+3 + 3
+
+# COMMAND ----------
+
+4 + 4
